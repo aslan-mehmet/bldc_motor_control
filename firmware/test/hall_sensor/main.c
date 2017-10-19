0320a05d83210e9b6fcee0299aa722170e47ce28
aslan-mehmet <aaslan.mehmet@hotmail.com>
2017-10-19 22:37:28 +0300

    hall_done

--- firmware/test/hall_sensor/main.c
+++ firmware/test/hall_sensor/main.c
@@ -32,18 +32,9 @@ int main(void)
 	uart_init();
 	hall_sensor_init();
 
-	uint8_t cnt = 0;
-	
-	while (1) {
-		USART_SendData(USART1, '0' + cnt);
-		if (cnt != 9) {
-			++cnt;
-		} else {
-			cnt = 0;
-		}
 
-		delay(1000);
-	}
+	while (1)
+		;
 }
 
 void uart_init(void)
@@ -107,15 +98,76 @@ void hall_sensor_init(void)
 
 /*
  * hall sensor input
- * pd10
- * pd11
- * pd12 
+ * pd10 - s3
+ * pd11 - s2
+ * pd12 - s1
  */
+
+	EXTI_InitTypeDef   EXTI_InitStructure;
+	GPIO_InitTypeDef   GPIO_InitStructure;
+	NVIC_InitTypeDef   NVIC_InitStructure;
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+	/* Enable SYSCFG clock */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+  
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOD, EXTI_PinSource10);
+	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOD, EXTI_PinSource11);
+	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOD, EXTI_PinSource12);
+	
+	
+	EXTI_InitStructure.EXTI_Line = EXTI_Line10;
+	EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+	EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising_Falling;
+	EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+	EXTI_Init(&EXTI_InitStructure);
+
+	EXTI_InitStructure.EXTI_Line = EXTI_Line11;
+	EXTI_Init(&EXTI_InitStructure);
+
+	EXTI_InitStructure.EXTI_Line = EXTI_Line12;
+	EXTI_Init(&EXTI_InitStructure);
+	
+	/* Enable and set EXTI15_10 Interrupt to the lowest priority */
+	NVIC_InitStructure.NVIC_IRQChannel = EXTI15_10_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x0F;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x0F;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+
+	NVIC_Init(&NVIC_InitStructure);
 
 }
 
 /* isr : read pins, print to uart */
+void EXTI15_10_IRQHandler(void)
+{
+	/* does not matter who gave the interrupt */
+	EXTI_ClearITPendingBit(EXTI_Line10 | EXTI_Line11 | EXTI_Line12);
 
+        uint16_t data = GPIO_ReadInputData(GPIOD);
+
+	data = (data >> 10) & 0x0007;
+	hall_current_step = data;
+
+	if (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == SET) {
+		/* dont expect to motor catch, playing safe */
+		USART_SendData(USART1, '0' + data);
+	}
+
+	GPIOD->ODR ^= LED_RED_ODR;
+}
 /* 
  * uart receive interrupt 
  * needed later
@@ -127,7 +179,7 @@ void USART1_IRQHandler(void)
 		USART_ClearITPendingBit(USART1, USART_IT_RXNE);
 		uint16_t data = USART_ReceiveData(USART1);
 		if (data == 'a') {
-			GPIOD->ODR ^= LED_GREEN_ODR;
+			GPIOD->ODR ^= LED_ORANGE_ODR;
 		}
 	}
 }
