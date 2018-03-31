42db287eafa2d0936544a604941454b8c8447b4f
Mehmet ASLAN <aaslan.mehmet@hotmail.com>
2018-03-31 21:27:17 +0300

    timed six step rotation, works

--- firmware/sensored_open_loop_f4_nucleo/main.c
+++ firmware/sensored_open_loop_f4_nucleo/main.c
@@ -13,8 +13,48 @@
 #include "ang_spd_sensor.h"
 #include "serial_packet_sent_cmd_ids.h"
 
-uint8_t _dma_transfer_done_flag = 0;
-uint8_t _adc_bemfs_readings[4];
+static uint16_t _pwm_val = 0;
+
+static void six_step_commutate_ccw(uint8_t state)
+{
+        ihm07_pwm_set_ch1(0);
+        ihm07_pwm_set_ch2(0);
+        ihm07_pwm_set_ch3(0);
+        ihm07_l6230_ch_all_disable();
+
+        switch (state) {
+        case 5:
+                ihm07_l6230_ch1_enable();
+                ihm07_l6230_ch2_enable();
+                ihm07_pwm_set_ch2(_pwm_val);
+                break;
+        case 4:
+                ihm07_l6230_ch1_enable();
+                ihm07_l6230_ch3_enable();
+                ihm07_pwm_set_ch3(_pwm_val);
+                break;
+        case 6:
+                ihm07_l6230_ch2_enable();
+                ihm07_l6230_ch3_enable();
+                ihm07_pwm_set_ch3(_pwm_val);
+                break;
+        case 2:
+                ihm07_l6230_ch1_enable();
+                ihm07_l6230_ch2_enable();
+                ihm07_pwm_set_ch1(_pwm_val);
+                break;
+        case 3:
+                ihm07_l6230_ch1_enable();
+                ihm07_l6230_ch3_enable();
+                ihm07_pwm_set_ch1(_pwm_val);
+                break;
+        case 1:
+                ihm07_l6230_ch2_enable();
+                ihm07_l6230_ch3_enable();
+                ihm07_pwm_set_ch2(_pwm_val);
+                break;
+        }
+}
 
 int main(void)
 {
@@ -40,32 +80,38 @@ int main(void)
         uint64_t hold_time = get_time();
         /* write after this line */
 
+        ihm07_pwm_and_pins_init();
+        ihm07_l6230_enable();
+        ihm07_pwm_state(ENABLE);
+        ihm07_l6230_ch_all_disable();
+
         ihm07_analog_pins_init();
-        uint8_t adc_bemf_chs[3] = {IHM07_ADC_CH_BEMF1, IHM07_ADC_CH_BEMF2, IHM07_ADC_CH_BEMF3};
-        ihm07_adc_dma_group_mode_init(adc_bemf_chs, _adc_bemfs_readings + 1, 3);
-        ihm07_adc_dma_interrupt_init();
-        ihm07_adc_dma_interrupt_connection_state(ENABLE);
-        ihm07_adc_dma_state(ENABLE);
+        ihm07_adc_single_mode_init(IHM07_ADC_CH_POT);
         ihm07_adc_state(ENABLE);
 
-        six_step_hall_init();
-        six_step_hall_set_pwm_val(500);
-        six_step_hall_start();
+        uint8_t six_step_lut[6] = {5, 4, 6, 2, 3, 1};
+        uint64_t hold_time_for_step = get_time();
+        uint8_t six_step_index = 0;
 
-        /* TODO: six_step_hall_init if not called, pwm timer clk not enabled */
-        /* FIX IT LATER */
-        ihm07_pwm_duty_interrupt_init();
-        ihm07_pwm_duty_interrupt_connection_state(ENABLE);
-        ihm07_pwm_duty_set_val(100);
-
-        _adc_bemfs_readings[0] = 0xaa;
-        uart6_stream_init(_adc_bemfs_readings, 4);
-        uart6_stream_start();
+        uint16_t pot_val[3];
+        uint16_t pot_val_avg;
 
         while (1) {
-                if (ang_spd_sensor_exist_new_value()) {
-                        float f = ang_spd_sensor_get_in_rpm();
-                        serial_packet_encode_poll(PRINT_SPD_RPM, sizeof(float), &f);
+                pot_val[0] = ihm07_adc_single_read_channel(IHM07_ADC_CH_POT);
+                pot_val[1] = ihm07_adc_single_read_channel(IHM07_ADC_CH_POT);
+                pot_val[2] = ihm07_adc_single_read_channel(IHM07_ADC_CH_POT);
+                pot_val_avg = (uint16_t) ((pot_val[0] + pot_val[1] + pot_val[2]) / 3.0);
+                _pwm_val = (uint16_t) (pot_val_avg * 1000 / 4095.0);
+                serial_packet_encode_poll(PRINT_PWM_VAL, sizeof(uint16_t), &_pwm_val);
+
+                if (get_time() - hold_time_for_step > 10) {
+                        hold_time_for_step = get_time();
+
+                        six_step_commutate_ccw(six_step_lut[six_step_index++]);
+
+                        if (six_step_index > 5) {
+                                six_step_index = 0;
+                        }
                 }
 
                 /* dont touch this lines */
@@ -77,16 +123,7 @@ int main(void)
         }
 }
 
-void ihm07_pwm_duty_interrupt_callback(void)
-{
-        ihm07_adc_start_conversion();
-}
-
-void ihm07_adc_dma_transfer_complete_callback(void)
-{
-        _dma_transfer_done_flag = 1;
-}
-
+/* dont touch this lines */
 void serial_packet_print(uint8_t byt)
 {
         uart_send_byte_poll(byt);
